3c1965001e4f33560b0c44ee023b56ede84143d8
Giorgi Javrishvili <giorgi@codegear.io>
2020-04-26 00:36:38 +0400

    1.py and functions review

--- 1.py
+++ 1.py
@@ -8,9 +8,18 @@ This is a temporary script file.
 #def hello(z):
 print("Hello Lammers!!!")
 #hello(z)
-far = input("Enter farenheit ")
+
+## Date: 26.04.2020 
+## Contributor: Giorgi
+### This code raises an error on first temp var... TypeError: unsupported operand type(s) for -: 'str' and 'int'
+### Reason: 'input' function returns a String in Python3, 
+### so we have to convert it to int if we are going to use returned value for math functions later
+### int(some_string)
+far = input("Enter farenheit ") 
 print ("Entered data is " + far)
-tmp = (far-32)/2
+### in our case int(far)
+tmp = (int(far)-32)/2 
 tmp = float(tmp)
 
 print (tmp)
+## შენ რომელი ვერსია Python გაქვს, შეცდომას არ აგდებდა?
--- Functions.py
+++ Functions.py
@@ -1,14 +1,14 @@
 def square(n):
   """Returns the square of a number."""
   squared = n ** 2
-  print "%d squared is %d." % (n, squared)
+  print ("%d squared is %d." % (n, squared)) ## გიო: შეცდომას აგდებდა სანამ ფრჩხილებში არ ჩავსვი
   return squared
   
 # Call the square function on line 10! Make sure to
 # include the number 10 between the parentheses.
 
 my_number_squared = square(10)
-print my_number_squared  # Prints 100 
+print (my_number_squared)  # Prints 100 ## გიო: იგივე აქაც, ფრჩხილები print-ს
 #print square(10)  # Also prints 100!
 #ფუნქცია ამუშავებს ცალკე პროგრამას, მისი დაძახება ხდება გარედან და მნიშვნელობა შეაქვს დაძახებისას
 
@@ -16,7 +16,7 @@ print my_number_squared  # Prints 100
 #აქ ძირი და ხარისხი პირდაპირ ფუნქციაშია მოცემული, ხოლო არგუმენტების შეყვანა ხდება ბოლოს ფუქციის დაძახებისას
 def power(base, exponent):  # Add your parameters here!
   result = base ** exponent
-  print "%d to the power of %d is %d." % (base, exponent, result)
+  print ("%d to the power of %d is %d." % (base, exponent, result))
 
 power(10, 4)  # Add your arguments here!
 
@@ -27,7 +27,7 @@ def one_good_turn(n):
 def deserves_another(n):
   return one_good_turn(n) + 2
 result = deserves_another(4)
-print result
+print (result)
 
 
 def cube(number):
@@ -41,7 +41,7 @@ def by_three(number):
     return False
 
 
-# ეს რაც ვერ გავიგე წესიერად
+# ეს რაც ვერ გავიგე წესიერად   ## რა ვერ გაიგე აქ?
 def shut_down(s):
   if s == "yes":
     return "Shutting down"
@@ -51,7 +51,7 @@ def shut_down(s):
   else:
     return "Sorry"
 #shut_down('yes')
-print shut_down
+print (shut_down)
 
 
 def hotel_cost(nights):
@@ -66,9 +66,9 @@ def plane_ride_cost(city):
   elif city == "Los Angeles":
     return 475
   else:
-    print "Wrong city"
+    print ("Wrong city")
 	
-	
+## ორჯერ რატომ გაქვს ერთიდაიგივე ფუნქციები?	
 def hotel_cost(nights):
   return 140 * nights
 
@@ -92,5 +92,16 @@ def rental_car_cost(days):
 def trip_cost(city, days, spending_money):
   return hotel_cost(days -1) + plane_ride_cost(city) + rental_car_cost(days) + spending_money
  
- print trip_cost("Los Angeles", 5, 800) #აქ ვიძახებთ trip_cost-ს და მნიშვნელობები შემოდის რიგითობით და შეესებამება 
- 
+print (trip_cost("Los Angeles", 5, 800)) #აქ ვიძახებთ trip_cost-ს და მნიშვნელობები შემოდის რიგითობით და შეესებამება 
+## აქ IndentationError: unindent does not match any outer indentation level ამ შეცდომას აგდებდა. 
+## იმიტომ, რომ print ერთი პრაბელით იყო გაწეული. ხომ იცი python-ში intendation-ის დიდი მნიშვნელობა აქვს. ზედმეტი space ცვლის ყველაფერს.
+## ჰოდა კიდე პრინტი ფრჩხილებში.
+## მთლიანად ეს კოდი აბრუნებს შემდეგს:
+""" 
+10 squared is 100.
+100
+10 to the power of 4 is 10000.
+7
+<function shut_down at 0x000002D5935E8D08>
+2015
+"""
