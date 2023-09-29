1493bb1235e6bdd46afe98d623b9555194d19050
Zjikia <37506123+Zjikia@users.noreply.github.com>
2023-09-30 00:06:45 +0400

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -4,6 +4,7 @@ Created on Fri Jul 17 17:28:29 2020
 
 @author: zjiki
 """
+# This file prints the date without time. if  needs exact date we have to put it in.
 
 import calendar
 print(calendar.month(2020, 9))
